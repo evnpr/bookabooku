682e230d16fef7e4bf25e861d07b3625f15a32e7
evanpy <evanpurnama5@gmail.com>
2012-06-27 08:55:34 +0800

    a

--- bookabooku/wsgi.py
+++ bookabooku/wsgi.py
@@ -26,8 +26,8 @@ framework.
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'letspan.settings'
-path = '/home/ubuntu/www/letspan/'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bookabooku.settings'
+path = '/home/ubuntu/www/bookabooku/'
 if path not in sys.path:
     sys.path.append(path)
 import django.core.handlers.wsgi
